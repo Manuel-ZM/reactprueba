111554ee1bd3cd8fc00bb1bba51dc9be016fe322
Manuel Zuñiga <manuelzuniga.w@gmail.com>
2022-05-31 18:46:57 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [inputFields, setInputFields] = useState([
+    { dato: '', input: '', type: '' }
+  ])
+
+const handleFormChange = (index, event) => {
+   let data = [...inputFields];
+   data[index][event.target.name] = event.target.value;
+   setInputFields(data);
+}
+
+const addFields = () => {
+  let newfield = { dato: '', input: '' }
+  setInputFields([...inputFields, newfield])
+}
+
+const submit = (e) => {
+  e.preventDefault();
+  JSON.stringify(inputFields)
+  console.log(inputFields)
+}
+
+
+const removeFields = (index) => {
+  let data = [...inputFields];
+  data.splice(index, 1)
+  setInputFields(data)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submit}>
+        {inputFields.map((input, index) => {
+          return (
+            <div key={index}>
+              <input
+                name='dato'
+                placeholder='Dato'
+                value={input.dato}
+                onChange={event => handleFormChange(index, event)}
+              />
+              <input
+               type={input.type}
+                name='input'
+                placeholder='Input'
+                value={input.input}
+                onChange={event => handleFormChange(index, event)}
+              />   
+                <input
+                name='type'
+                placeholder='Tipo'
+                value={input.type}
+                onChange={event => handleFormChange(index, event)}
+              />   
+
+
+              <button onClick={() => removeFields(index)}>Quitar</button>
+            </div>    
+          )
+        })}
+              <button onClick={addFields}>Agregar</button>
+              
+              <button onClick={submit}>Guardar</button>
+      </form>
+      <div>
+        <link url="https://www.w3schools.com/html/html_form_input_types.asp"> los tipos de input de datos</link>
+      </div>
     </div>
   );
 }
 
 export default App;
+

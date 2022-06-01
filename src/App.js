ff2476d8d7fe5cc79ae59d9f5f760b5790b088be
Manuel Zuñiga <manuelzuniga.w@gmail.com>
2022-05-31 23:41:33 -0500

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const handleFormChange = (index, event) => {
 }
 
 const addFields = () => {
-  let newfield = { dato: '', input: '' }
+  let newfield = { dato: '', input: '', type: ''}
   setInputFields([...inputFields, newfield])
 }
 
@@ -33,42 +33,60 @@ const removeFields = (index) => {
 }
 
   return (
-    <div className="App">
-      <form onSubmit={submit}>
+    <div className="App" class="w-full">
+      
+<div class="text-black align-middle items-center text-center p-2 w-1/2 rounded bg-gray-800">
+<p class="block text-black text-bold items-center text-center px-2"> 
+<b class="text-center text-zinc-400">Base de Datos</b>
+</p>
         {inputFields.map((input, index) => {
           return (
-            <div key={index}>
-              <input
+            <div class="space-x-2 space-y-1" key={index}>
+              <input class='inline-block rounded bg-red-700 text-center space-y-1 space-x-2 border-2'
                 name='dato'
                 placeholder='Dato'
                 value={input.dato}
                 onChange={event => handleFormChange(index, event)}
               />
-              <input
+              <input class='inline-block rounded bg-red-700 text-center space-y-1 space-x-2 border-2'
                type={input.type}
                 name='input'
                 placeholder='Input'
                 value={input.input}
                 onChange={event => handleFormChange(index, event)}
               />   
-                <input
+              <input class='inline-block rounded bg-red-700 text-center space-y-1 space-x-2 border-2'
                 name='type'
-                placeholder='Tipo'
+                placeholder='Tipo de Dato'
                 value={input.type}
                 onChange={event => handleFormChange(index, event)}
-              />   
+              />
 
 
-              <button onClick={() => removeFields(index)}>Quitar</button>
+              <button class="text-bold rounded bg-slate-500 px-2 hover:border-2 hover:text-white" onClick={() => removeFields(index)}>Quitar</button>
             </div>    
           )
         })}
-              <button onClick={addFields}>Agregar</button>
-              
-              <button onClick={submit}>Guardar</button>
-      </form>
+        <div class="my-2"></div>
+              <button class="text-bold rounded bg-slate-500 px-2 border-2 py-2 hover:bg-opacity-50" onClick={addFields}>Agregar</button>
+              <a class="mx-2"></a>
+              <button class="text-bold rounded bg-slate-500 px-2 border-2 py-2 hover:bg-opacity-50" onClick={submit}>Guardar</button>
+
       <div>
-      <a href="https://www.w3schools.com/html/html_form_input_types.asp" className=' text-red-500'> los tipos de input de datos</a>
+      </div>
+      <div class="absolute right-0 top-0 w-1/2 p-2">
+      <a href="https://www.w3schools.com/html/html_form_input_types.asp" target="_blank" rel="noopener noreferrer" class="rounded border-2 p-2 bg-slate-500 text-white hover:bg-slate-800">Los tipos de input de datos</a>
+      
+      <a href="https://github.com/Manuel-ZM/reactprueba" target="_blank" rel="noopener noreferrer" class="rounded border-2 p-2 bg-slate-500 text-white hover:bg-slate-800">Github</a>
+      <br></br>
+      <p class='font-bold break py-2'>pide usar _uid me comentaron solo usar ReactJs y Tailwind, _uid es una libreria, tampoco puedo hacer que el Json se vea "bonito" es otra libreria</p>
+      </div>
+      <div>
+      <div class="absolute right-0 bottom-1/2 items-center px-2 rounded align-middle break w-1/3 bg-slate-400">
+        <a>Json</a>
+        {JSON.stringify(inputFields, null, 2)}
+        </div>
+      </div>
       </div>
     </div>
   );
